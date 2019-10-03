b6839835a8b46f368911fb37d6cbd54298de4fba
Kaushik <Kaushiks90@gmail.com>
2019-10-03 09:21:43 +0530

    Swagger versioning

---
+++ DotNetCore-Architecture/Installer/DbInstaller.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DotNetCore_Architecture.Installer
+{
+    public class DbInstaller : IInstaller
+    {
+        public void IInstallServices(IConfiguration configuration, IServiceCollection services)
+        {
+            services.AddDbContext<Data.DbContext>(options =>
+               options.UseSqlServer(
+                   configuration.GetConnectionString("DefaultConnection")));
+            services.AddDefaultIdentity<IdentityUser>()
+                .AddEntityFrameworkStores<Data.DbContext>();
+        }
+    }
+}
---
+++ DotNetCore-Architecture/Installer/MvcInstaller.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DotNetCore_Architecture.Installer
+{
+    public class MvcInstaller : IInstaller
+    {
+        public void IInstallServices(IConfiguration configuration, IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddSwaggerGen(x =>
+            {
+                x.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "Our Api", Version = "v1" });
+            });
+        }
+    }
+}
