27687f6fcbe855091b6ba65090dc160437038b7c
Kaushik <Kaushiks90@gmail.com>
2019-10-06 21:18:47 +0530

    Integration Testing

--- DotNetCore-Architecture/Options/JwtSettings.cs
+++ DotNetCore-Architecture/Options/JwtSettings.cs
@@ -8,6 +8,6 @@ namespace DotNetCore_Architecture.Options
     public class JwtSettings
     {
         public string Secret { get; set; }
-        public TimeSpan TokenLifetime { get; set} 
+        public TimeSpan TokenLifetime { get; set; } 
     }
 }
---
+++ IntegrationTest/PostsControllerTests.cs
@@ -0,0 +1,48 @@
+﻿using DotNetCore_Architecture.Contracts;
+using DotNetCore_Architecture.Contracts.V1.Requests;
+using DotNetCore_Architecture.Domain;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace IntegrationTest
+{
+    public class PostsControllerTests : IntegrationTest
+    {
+        [Fact]
+        public async Task GetAll_WithoutAnyPosts_ReturnsEmptyResponse()
+        {
+            // Arrange
+            await AuthenticateAsync();
+
+            // Act
+            var response = await TestClient.GetAsync(ApiRoutes.Posts.GetAll);
+
+            // Assert
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+            (await response.Content.ReadAsAsync<List<Post>>()).Should().BeEmpty();
+        }
+
+        [Fact]
+        public async Task Get_ReturnsPost_WhenPostExistsInTheDatabase()
+        {
+            // Arrange
+            await AuthenticateAsync();
+            var createdPost = await CreatePostAsync(new CreatePostRequest { Name = "Test post" });
+
+            // Act
+            var response = await TestClient.GetAsync(ApiRoutes.Posts.Get.Replace("{postId}", createdPost.Id.ToString()));
+
+            // Assert
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+            var returnedPost = await response.Content.ReadAsAsync<Post>();
+            returnedPost.Id.Should().Be(createdPost.Id);
+            returnedPost.Name.Should().Be("Test post");
+        }
+    }
+}
